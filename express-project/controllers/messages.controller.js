95e31d98784c3acad0712c37dee3176514abc9bc
Faraz <farazgojali@gmail.com>
2023-01-09 16:33:00 +0500

    public folder changes in project

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 function getMessages(req, res) {
 
-    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
-    // res.send('<ul><li>Hello Albert!</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
 }
 
 function postMessage(req, res) {
