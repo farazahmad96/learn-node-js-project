8c66fd6ed0cfd12fd308870934714808dc5e2f64
Faraz <farazgojali@gmail.com>
2023-01-09 17:42:42 +0500

    layouts and separations

--- express-project/server.js
+++ express-project/server.js
@@ -8,6 +8,9 @@ const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -19,9 +22,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/site', express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My Friends are Clever',
+        caption: 'Let\'s go skiing!',
+    });
+});
 app.use('/friends', friendsRouter);
 app.use('/messages', messagesRouter);
 
